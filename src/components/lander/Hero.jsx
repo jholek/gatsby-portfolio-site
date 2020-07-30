ef2042eccbedb685ef3c4af650d484ea5018cca2
Jeff Holek <jeff.holek@gmail.com>
2020-07-29 21:57:00 -0400

    added html elements for hero section

--- src/components/lander/Hero.jsx
+++ src/components/lander/Hero.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-const Hero = () => <header>Hero Section</header>;
+const Hero = () => (
+  <header>
+    Hero Section
+    <article>
+      <h6>Hello, I'm Jeff</h6>
+      <Link to='/about/'>More About Me --></Link>
+      <h1>Let's talk product.</h1>
+      <p>
+        Whether it’s bringing a vision to life, delighting users or simply
+        trying to raise the bar, I work alongside organizations to find novel
+        solutions to everyday challenges.
+      </p>
+      <p>Have a project in mind?</p>
+      <button>Let's Chat</button>
+    </article>
+  </header>
+);
 
 export default Hero;
