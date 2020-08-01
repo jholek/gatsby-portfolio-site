151a005bc40861b0d942d63f03cc9f411e4d1c77
Jeff Holek <jeff.holek@gmail.com>
2020-07-31 21:09:56 -0400

    added gatsby plugins, added hero img

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,27 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'assets',
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+    // {
+    //   resolve: 'gatsby-source-strapi',
+    //   options: {
+    //     apiURL: 'http://localhost:1337',
+    //     queryLimit: 1000, // Default to 100
+    //     //   contentTypes : `jobs`, `projects`, `blogs`,
+    //     //   singleType : `about`
+    //     //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
+    //     contentTypes: [],
+    //     singleTypes: ['about'],
+    //   },
+    // },
+  ],
+};
--- src/components/lander/Hero.jsx
+++ src/components/lander/Hero.jsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import Image from 'gatsby-image';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 
 import SocialLinks from '../SocialLinks';
 
-const Hero = () => (
-  <header>
-    Hero Section
-    <article>
-      <h6>Hello, I'm Jeff</h6>
-      <Link to='/about/'>More About Me --></Link>
-      <h1>Let's talk product.</h1>
-      <p>
-        Whether it’s bringing a vision to life, delighting users or simply
-        trying to raise the bar, I work alongside organizations to find novel
-        solutions to everyday challenges.
-      </p>
-      <p>Have a project in mind?</p>
-      <button>Let's Chat</button>
-      <SocialLinks />
-    </article>
-  </header>
-);
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img-temp.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
+
+const Hero = () => {
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query);
+
+  return (
+    <header>
+      Hero Section
+      <article>
+        <h6>Hello, I'm Jeff</h6>
+        <Link to='/about/'>More About Me --></Link>
+        <h1>Let's talk product.</h1>
+        <p>
+          Whether it’s bringing a vision to life, delighting users or simply
+          trying to raise the bar, I work alongside organizations to find novel
+          solutions to everyday challenges.
+        </p>
+        <p>Have a project in mind?</p>
+        <button>Let's Chat</button>
+        <SocialLinks />
+        <Image fluid={fluid} />
+      </article>
+    </header>
+  );
+};
 
 export default Hero;
